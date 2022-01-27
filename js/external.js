d77e56349f4d83acbedad6b3cd238a3baf757df3
Jason Cameron <dev.jasoncameron@gmail.com>
2022-01-27 15:23:43 -0600

    JS1 Ex 3 files completed and added

---
+++ js/external.js
@@ -0,0 +1,15 @@
+
+// 1.
+console.log("Hello from external JavaScrip");
+
+alert("Welcome to my Website");
+
+// 2.A
+var answer = prompt("what is your favorite color");
+
+// 2.B
+if (answer === "green") {
+    alert("Great, green is my favorite color too!")
+}
+
+// 3???
