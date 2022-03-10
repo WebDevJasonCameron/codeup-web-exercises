9913ce55b95cad206ccef7c7bca5851e332fec9d
Jason Cameron <dev.jasoncameron@gmail.com>
2022-03-10 09:56:23 -0600

    update jq faq ex. Added images to complete the bonus (if I have the time to complete)

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -9,21 +9,54 @@ $(document).ready(function() {
         $(this).next('dd').toggleClass('highlight');
     })
 
+    // 3. Create a button that, when clicked, makes the last li in each
+    //    ul have a yellow background.
+
     $('#embolden').click(function () {
         $('li').each(function (i, e) {
             $(e).children().last().children().last().toggleClass('highlight');
         });
     })
 
+    // 4. When any h3 is clicked, the lis underneath it should be bolded.
+    //    Use font-weight: bold to achieve this.
+
     $('h3').click(function () {
-        $(this).next().children().first().toggleClass('make-bold');
-        console.log('hit')
+        $(this)
+            .next()
+            .first()
+            .toggleClass('make-bold');
     })
 
+    // 5. When any list item is clicked, first li of the parent ul should
+    // have a font color of blue.
     //  BROKEN ... FIX  (SO CLOSE THOUGH)
     $('li').click(function () {
-        $(this).parent().children().first().css('color', 'blue')
+        $(this).parent().children().first().css('color', 'blue');
     })
+
+
+    // Create 3 divs that each look like a picture frame. Each one should
+    // have a unique background image and a button underneath that swaps
+    // the image from the frame. Use the traversing functions of jQuery
+    // to accomplish this.
+    //
+    // The rules are the following:
+    //
+    // The left frame swaps to the right and takes the image from the frame in the center.
+    // The center frame swaps randomly to either the left or right.
+    // The right frame swaps to the left and takes the image from the frame in the center.
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
