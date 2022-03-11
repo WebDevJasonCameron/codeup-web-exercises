c3c526762a28be46ef5c8d2dcac4add11393f76a
Jason Cameron <dev.jasoncameron@gmail.com>
2022-03-11 08:02:52 -0600

    added ajax blog files (html, css, json, and js).  Built scafold in html

---
+++ js/ajax-blog.js
@@ -0,0 +1,29 @@
+// AJAX BLOG INSTRUCTIONS:
+// 1. Create a new html file called ajax-blog.html.
+// 2. At minimum, your Ajax blog will need an empty
+//    <div> with an id of posts.
+// 3. Add your own Bootstrap theme to this file as well.
+//    Feel free to use the same one as your store or
+//    choose a different one.
+// 4. Download blog.json to your data directory from before.
+// 5. Use Ajax to load the contents of blog.json and add the
+//    data from it to your #posts div.
+// 6. Add additional entries to blog.json and make sure your
+//    changes are reflected on the page.
+
+$(document).ready(function() {
+
+    $.get('data/blog.json')
+
+
+
+
+
+
+
+
+
+
+
+
+});
