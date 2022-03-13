035100a5d42b0eadc171444b85ff3b25265afa52
Jason Cameron <dev.jasoncameron@gmail.com>
2022-03-12 22:03:05 -0600

    completed the mini Ex for mailbox

--- js/mapbox-4.js
+++ js/mapbox-4.js
@@ -16,15 +16,44 @@
     let map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-98.4936, 29.4241],                    // Lincoln Memorial
-        zoom: 12
+        center: [-98.4936, 29.4241],                    // San Antonio
+        zoom: 10
     });
 
-    geocode("Paris, Texas", MAPBOX_API_TOKEN).then(function (data){
-        console.log(data);
+
+    function goToZionLL(){
+        geocode("Zion National Park", MAPBOX_API_TOKEN).then(function (data){
+            $('#info-here').text('Moved to Zion National Part at the position: ' +
+                data.join(' : '));
+            resetLocation(data);
+            map.setZoom(9)
+        })
+    }
+
+    function goToHawaii(){
+        reverseGeocode({lng: -155.5828, lat: 19.8968}, MAPBOX_API_TOKEN).then(function (results){
+            $('#info-here').text('Moving to' + results + ' at the position 155.5828 : 19.8968');
+            resetLocation([-155.5828, 19.8968]);
+            map.setZoom(7);
+
+        })
+    }
+
+    function resetLocation(position){
+        console.log('reset location is here: ' + position);
+        map.setCenter(position);
+    }
+
+
+    $('#zion-btn').click(function (){
+        goToZionLL();
+    })
+    $('#hawaii-btn').click(function (){
+        goToHawaii();
     })
 
 
 
-
 })();
+
+// 19.8968° N, 155.5828° W <-- Hawaii
