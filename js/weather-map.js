56f4f77d713e41ba69e4b3456bf7231288a9d20b
Jason Cameron <dev.jasoncameron@gmail.com>
2022-03-14 15:25:30 -0500

    wx map scafold complete. Notes on data location included below js. Updating card to take data

--- js/weather-map.js
+++ js/weather-map.js
@@ -17,43 +17,84 @@
         zoom: 9
     });
 
+    // Build Card HTML
+    function buildCardScript(obj){
+        return '' +
+            '        <div class="wx-card card mx-2"' +
+            '            <div class="card-header text-center"' +
+            '                <div id="wx-date" aria-label="Weather Date"' +
+                                obj.dt +
+            '                </div' +
+            '            </div' +
+            '            <div class="card-body"' +
+            '                <div class="wx-temp" aria-label="temperature">' +
+                                obj.temp.day +
+            '               </div' +
+            '                <img class="wx-img">' +
+                                'IMG ICON' +
+            '                </img' +
+            '                <hr' +
+            '                <div class="wx-description" aria-label="description">' +
+            '' +
+            '                   </div' +
+            '                <div class="wx-humidity" aria-label="humidity">HUMIDITY</div' +
+            '                <hr' +
+            '                <div class="wx-wind" aria-label="wind">WIND</div' +
+            '                <div class="wx-pressure" aria-label="pressure">PRESSURE</div' +
+            '            </div' +
+            '        </div>'
+    }
+
+    function loopThroughWxList(wxList){
+        for (let i = 0; i < (wxList.length -2); i++) {
+            console.log(wxList[i]);
+        }
+    }
+
 
     /**
      *  WX
      */
     // Get WX
-    $.get('https://api.openweathermap.org/data/2.5/onecall', {
-        lat: 29.4252,
-        lon: -98.4916,
-        appid: WEATHER_TOKEN,
-        exclude: 'minutely, hourly, current, alerts,',
-        units: 'imperial'
-    }).done(function (data){
-        console.log(data);
-    }).fail(function (jqXhr, status, error) {
-        console.log(jqXhr);
-        console.log(status);
-        console.log(error);
-    })
+    function getWeather(){
+        $.get('https://api.openweathermap.org/data/2.5/onecall', {
+            lat: 29.4252,
+            lon: -98.4916,
+            appid: WEATHER_TOKEN,
+            exclude: 'minutely, hourly, current, alerts,',
+            units: 'imperial'
+        }).done(function (data){
+            // console.log(data.daily);
+
+            loopThroughWxList(data.daily)
 
 
 
 
+        }).fail(function (jqXhr, status, error) {
+            console.log(jqXhr);
+            console.log(status);
+            console.log(error);
+        })
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /**
+     *  TEST
+     */
+    getWeather();
 
 
 })();
+
+// BASE                     .daily[0]                               <--Cycle Through
+// DATE:                        .dt
+// DAY'S TEMP:                  .temp.day
+// DESCRIPTION:                 .weather[0].description
+// IMG:                         .weather[0].icon
+// HUMIDITY:                    .humidity
+// WIND:
+//          DEG                     .wind_deg
+//          SPEED                   .wind_speed
+// PRESSURE:                    .pressure
+
