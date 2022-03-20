ff2b4832531b621d8af79115bcf27fcc22101af3
Jason Cameron <dev.jasoncameron@gmail.com>
2022-03-19 21:43:03 -0500

    worked in SB 09 on the topic of fetch and promises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -1,3 +1,9 @@
+"use strict";
+
+// 1. Create a file named map-filter-reduce.js in your js directory and
+//    copy the user's data below into it.
+//    <--DONE
+
 const users = [
     {
         id: 1,
@@ -35,3 +41,60 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+// 2. Use .filter to create an array of user objects where each user object
+//    has at least 3 languages in the languages array.
+console.log('Problem 02.........................');
+const threeLang = users.filter(({languages}) => languages.length >= 3);
+
+console.log(threeLang);
+
+
+// 3. Use .map to create an array of strings where each element is a user's
+//    email address
+console.log('Problem 03.........................');
+const userEmailList = users.map(({email}) => email);
+
+console.log(userEmailList);
+
+
+// 4. Use .reduce to get the total years of experience from the list of users.
+//    Once you get the total of years you can use the result to calculate the
+//    average.
+console.log('Problem 04.........................');
+const totalYearsExp = users.reduce((pv, cv,) => pv + cv.yearsOfExperience, 0);
+console.log('Total years user exp: ' + totalYearsExp);
+const totalAverageExp = users.reduce((pv, cv, i, a) =>
+    pv + cv.yearsOfExperience / a.length - 1, 0);
+
+console.log('Total average exp: ' + totalAverageExp);
+
+
+
+// 5. Use .reduce to get the longest email from the list of users.
+console.log('Problem 05.........................');
+const longestEmail = users.reduce((pv, cv) => {
+    if(cv.email.length > pv.length)
+        pv = cv.email;
+    return pv
+}, '');
+
+console.log('Longest email is: ' + longestEmail);
+
+
+// 6. Use .reduce to get the list of user's names in a single string. Example:
+//    Your instructors are: ryan, luis, zach, fernando, justin.
+console.log('Problem 06.........................');
+const userNamesInString = users.reduce((pv, cv, i, a) =>
+    pv + cv.name + (i < (a.length - 1) ? ', ' : '.')                                    //   <-- TRICKY!!!
+, '');
+
+console.log('User names in a string: ' + userNamesInString);
+
+
+
+
+
+
+
+
--- sandbox/sandbox_09.js
+++ sandbox/sandbox_09.js
@@ -1,38 +1,36 @@
-// 9.
-function checkV(x){
-    switch (x.toLowerCase()){
-        case 'a': case 'e': case 'i': case 'o': case 'u':
-            return true;
-    }
+/**
+ *      USING FETCH WITH GITHUB API
+ */
+
+// fetch('https://api.github.com/users').then( response => {
+//     response.json().then( users => {
+//         users.forEach( user => {
+//             // Do something with the user object
+//             console.log(user);
+//         })
+//     })
+// })
+
+// fetch('https://api.github.com/users').then( response => {
+//     response.json().then( users => {
+//         users.forEach( user => {
+//             // Do something with the user object
+//             console.log(user.login);
+//         })
+//     })
+//         .catch(error => console.error('Hey, you got an error'))
+// })
+
+// NOW STORE IT INTO A VAR
+
+function getGithubUsernames() {
+    return fetch('https://api.github.com/users')
+        .then(response => response.json());
 }
-function countVowels(s){
-    let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        if(checkV(s[i]) === true) count ++;
-    }
-    return count;
-}
 
-// console.log(countVowels('zyz'));
-
-
-
-
-
-// 11.
-function capitalizeName(s){
-    s = s.split(' ');
-    let fn = s[0].toString().charAt(0).toUpperCase() + s[0].toString().slice(1);
-    let ln = s[1].toString().charAt(0).toUpperCase() + s[1].toString().slice(1);
-
-    return fn + ' ' + ln;
-}
-
-console.log(capitalizeName('calvin cameron'));
-
-
-
-
-
-
-
+getGithubUsernames().then( users => {
+    users.forEach( userObj => {
+        // do something with each username
+        console.log(userObj.login);
+    })
+}).catch(error => console.log(error));
